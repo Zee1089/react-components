3ba6b51babe8f38b11cb5c6f8bcbcc662716c948
Zee <zeeboo696@gmail.com>
2024-07-17 09:43:26 -0400

    understanding react as components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 // src/App.jsx
 
+import Navbar from './components/Navbar/Navbar.jsx';
+import FounderListItem from './components/FounderListItem/FounderListItem.jsx';
+
 import './App.css';
 
 const App = () => {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <Navbar />
+      <main>
+        <h1>Welcome to Hyperbo.ly</h1>
+        <h2>The future is firmly in front of us!</h2>
+        <p>If you can't explain it to a fifth-grader, our product does it.</p>
+      </main>
+      <h2>Our Founders</h2>
+      <ul>
+        <FounderListItem />
+        <li>
+          <h3>Sam Bankman-Fried: CFO</h3>
+          <p>CPA from TotallyReal State</p>
+        </li>
+        <li>
+          <h3>Matt Damon: CMO</h3>
+          <p>Was in that Movie You saw</p>
+        </li>
+      </ul>
+    </>
   );
 };
-
-export default App;
---
+++ src/components/FounderListItem/FounderListItem.jsx
@@ -0,0 +1,12 @@
+// src/components/FounderListItem/FounderListItem.jsx
+
+const FounderListItem = () => {
+    return (
+      <li>
+        <h3>Hello, I'm a super impressive Founder!</h3>
+      </li>
+    );
+  };
+  
+  export default FounderListItem;
+  
